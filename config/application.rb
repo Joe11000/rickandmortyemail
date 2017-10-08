15d530d92f9940890faae528d85793573c3276d7
Joe Noonan <joenoonan27@gmail.com>
2017-10-08 01:52:59 -0500

    production still failing

--- config/application.rb
+++ config/application.rb
@@ -15,15 +15,6 @@ config.action_mailer.perform_deliveries = true
 
 # config.action_mailer.default_url_options = {host: '0.0.0.0:3000'}
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            ENV['gmail_user_name'],
-    password:             ENV['gmail_password'],
-    authentication:       'plain',
-    enable_starttls_auto: true  }
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
