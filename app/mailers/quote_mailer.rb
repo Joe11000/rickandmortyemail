4f6b8b622fa0725fed8bf42f078370141cec50f1
Joe Noonan <joenoonan27@gmail.com>
2017-10-18 12:55:42 -0500

    rename some variables

--- app/mailers/quote_mailer.rb
+++ app/mailers/quote_mailer.rb
@@ -6,14 +6,15 @@ class QuoteMailer < ApplicationMailer
 
   def quote_email user
     @user = user
-    @quote = Faker::RickAndMorty.quote
+    @random_quote = Faker::RickAndMorty.quote
+    @random_character = Faker::RickAndMorty.character
 
     google_search_url = "https://www.google.com/search?q=rick+and+morty+pictures&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiviZ7L6_jWAhWCxIMKHRMYAdcQ_AUICigB&biw=720&bih=775"
     @random_google_image_url = Nokogiri::HTML(open(google_search_url)).css('#center_col img').to_a.sample['src']
-    @character = Faker::RickAndMorty.character
 
-    rick_pick_asset_path = Dir['public/assets/rick_pic*jpg'].try(:first)
-    attachments['rick_pick.jpg'] = File.read(rick_pick_asset_path)
+    # assign attachment picture to email
+      rick_pick_asset_path = Dir['public/assets/rick_pic*jpg'].try(:first)
+      attachments['rick_pick.jpg'] = File.read(rick_pick_asset_path)
 
     mail(to: @user.email, subject: "Rick and Morty: Good Morning #{@user.name}!")
   end
